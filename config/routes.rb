bb099a5fea896738b43d45cd2650b0d923648cf5
Scott <z89quark@gmail.com>
2014-03-04 00:00:56 +0000

    add a stati-pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Hartl::Application.routes.draw do
+  get "foo_bar/baz"
+  get "foo_bar/qux"
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/views/foo_bar/qux.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "foo_bar/qux.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
